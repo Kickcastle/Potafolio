b7cc69ff84eb46b43dd89396f3d92ac5434ecf18
Kickcastle <heen.ventura@gmail.com>
2021-05-29 11:47:28 -0700

    Se mejoras en la galeria, responsive y nav

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function galeriaAI() {
+    elemento = document.getElementById("galeria-ai");
+    elemento.style.display = "none";
+
+}
+function galeriaAI2() {
+    elemento = document.getElementById("galeria-ai");
+    elemento.style.display = "flex";
+
+}
