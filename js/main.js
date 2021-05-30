2024786fdd503fabd35aa80922da0bfb2074dbc8
Kickcastle <heen.ventura@gmail.com>
2021-05-30 12:29:19 -0700

    Se modificaron pages, se agrego el nuevo icono de menu, reformas de contact form

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ function galeriaAI() {
     elemento = document.getElementById("galeria-ai");
     elemento.style.display = "none";
 
+    if (galeriaAI() == true) {
+        galeriaAI();
+    }else {
+        galeriaAI2();
+    }
 }
 function galeriaAI2() {
     elemento = document.getElementById("galeria-ai");
     elemento.style.display = "flex";
 
 }
+
+
